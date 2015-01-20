89f4871b5c024d6a69ed636c83803dccc2f9af7c
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-01-20 17:43:58 -0500

    Updated free for all exercise

---
+++ lib/conway.rb
@@ -0,0 +1,16 @@
+require 'byebug'
+class Cell
+  def initialize(x,y)
+    @x = x
+    @y = y
+  end
+
+  def find_position
+    @x == 0 && @y == 0
+
+# binding.byebug
+
+
+  end
+
+end
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,1 +1,14 @@
 require_relative '../lib/conway'
+require 'byebug'
+
+#describe class called Cell
+describe Cell do
+
+  describe '#find_position' do
+    cell = Cell.new(0,0)
+    it 'returns true if cell is the first cell in grid' do
+      expect(cell.find_position).to eq true
+    end
+  end
+
+end
